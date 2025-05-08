9e44815de274d2cb7e12a6b4af3d65cfb826db38
CVCldwll <cvcucr@gmail.com>
2025-05-07 21:45:58 -0700

    first commit

---
+++ Ch1/1.2/first.cpp
@@ -0,0 +1,9 @@
+// This program outputs the message "Hello, World!" to the monitor
+
+import std;         // gain access to the C++ standard library
+
+int main()
+{
+    std:cout << "Hello, World\n";
+    return 0;
+}
